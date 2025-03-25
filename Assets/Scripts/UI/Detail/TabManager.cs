b46d3d9475593622d0fe384e2a6ebe12e13674f3
koo4802 <koo4802@gmail.com>
2025-03-25 21:57:14 +0900

    UserDetail  Scene UI 로직 일부 구현

---
+++ Assets/Scripts/UI/Detail/TabManager.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TabManager : MonoBehaviour
+{
+    [SerializeField] public GameObject onPickedEffect; // 선택 효과
+    [SerializeField] private GameObject dashboardComponent;
+    [SerializeField] private GameObject selectComponent;
+    [SerializeField] private GameObject mystockComponent;
+
+    private Vector3[] tabPositions = new Vector3[]
+    {
+        new Vector3(0, 0, 0),
+        new Vector3(0, -110, 0),
+        new Vector3(0, -220, 0)
+    };
+
+    // 선택된/선택되지 않은 텍스트 색상
+    private Color selectedColor = new Color32(0x64, 0xFF, 0xDA, 0xFF); // 0x80은 255의 50%인 128
+    private Color unselectedColor = new Color(0.5f, 0.5f, 0.5f); // 회색
+
+    void Start()
+    {
+        // 시작할 때 모든 텍스트 색상을 unselectedColor로 초기화
+        SetTabTextColors(0);  // 0번(Dashboard) 탭이 선택된 상태로 시작
+
+        if (onPickedEffect != null)
+        {
+            onPickedEffect.SetActive(true);
+            // 컴포넌트 활성화/비활성화
+            dashboardComponent.SetActive(true);
+            selectComponent.SetActive(false);
+            mystockComponent.SetActive(false);
+            // 이펙트 이동
+            MoveEffectToTab(0);
+        }
+    }
+
+    private void SetTabTextColors(int selectedTab)
+    {
+        try 
+        {
+            var dashboardText = GameObject.Find("dashboard_txt");
+            var selectText = GameObject.Find("select_txt");
+            var mystockText = GameObject.Find("mystock_txt");
+
+            var dashboardTMP = dashboardText.GetComponent<TMP_Text>();
+            var selectTMP = selectText.GetComponent<TMP_Text>();
+            var mystockTMP = mystockText.GetComponent<TMP_Text>();
+
+            // 모든 텍스트를 60% 불투명도의 흰색으로 초기화 (alpha: 153)
+            Color defaultColor = new Color32(255, 255, 255, 153);
+            dashboardTMP.color = defaultColor;
+            selectTMP.color = defaultColor;
+            mystockTMP.color = defaultColor;
+
+            // 선택된 탭의 텍스트만 색상 변경
+            switch(selectedTab)
+            {
+                case 0:
+                    dashboardTMP.color = selectedColor;
+                    break;
+                case 1:
+                    selectTMP.color = selectedColor;
+                    break;
+                case 2:
+                    mystockTMP.color = selectedColor;
+                    break;
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Error in SetTabTextColors: {e.Message}");
+        }
+    }
+
+    public void OnClickDashboardTab()
+    {
+        MoveEffectToTab(0);
+        SetTabTextColors(0);
+        dashboardComponent.SetActive(true);
+        selectComponent.SetActive(false);
+        mystockComponent.SetActive(false);
+    }
+
+    public void OnClickSelectTab()
+    {
+        MoveEffectToTab(1);
+        SetTabTextColors(1);
+        dashboardComponent.SetActive(false);
+        selectComponent.SetActive(true);
+        mystockComponent.SetActive(false);
+    }
+
+    public void OnClickMyStockTab()
+    {
+        MoveEffectToTab(2);
+        SetTabTextColors(2);
+        dashboardComponent.SetActive(false);
+        selectComponent.SetActive(false);
+        mystockComponent.SetActive(true);
+    }
+
+    private void MoveEffectToTab(int tabIndex)
+    {
+        if (onPickedEffect != null)
+        {
+            onPickedEffect.GetComponent<RectTransform>().anchoredPosition = tabPositions[tabIndex];
+        }
+    }
+}
